f5e86799cbf1fd6957be9f7c53aff4ea6de20775
Marco <marco.basilio@albsolution.com.br>
2024-09-16 09:57:52 -0300

    Create db, and the logic of message/conversation

--- backend/app/api/endpoints/login_auth.py
+++ backend/app/api/endpoints/login_auth.py
@@ -1,19 +1,15 @@
-# backend/app/api/endpoints/login_auth.py
-
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
-from app.core.database import connection
+from app.core.login_database import fetch_all_conversations
 
 router = APIRouter()
 
 @router.get("/login")
 def login(email: str, password: str):
     try:
-        query = connection()
-        if query:
-            for user in query:
-                if user[1] == email and user[2] == password:
-                    return JSONResponse(content={"message": "Login successful"}, status_code=200)
+        user = fetch_all_conversations()
+        if user and user[2] == password: 
+            return JSONResponse(content={"message": "Login successful"}, status_code=200)
         raise HTTPException(status_code=401, detail="Invalid credentials")
     except Exception as e:
         print(f"Exception occurred: {e}")
--- backend/app/api/endpoints/message_chat.py
+++ backend/app/api/endpoints/message_chat.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from app.codes.send_message import ApiRequest
+from app.core.conv_database import store_conversation, location_contacts
+from app.core.contact_database import resgister_contact
+
 
 router = APIRouter()
 
@@ -17,11 +20,21 @@ async def message(request: MessageRequest):
     try:
         api_request = ApiRequest()
         response_data = api_request.send_messages(request.number, request.textMessage.text)
+        
+        resgister_contact('paulo', request.number)
+        
+        contact_ids = location_contacts(int(request.number))
+        if not contact_ids:
+            raise HTTPException(status_code=404, detail="Contact not found.")
+        
+        contact_id = contact_ids[0][0] 
+        
+        store_conversation(contact_id, request.textMessage.text, "send")
         return JSONResponse(content={"message": "Message sent successfully!", "response": response_data}, status_code=200)
+
     except HTTPException as e:
         print(f"HTTPException: {e}")
         raise e
     except Exception as e:
         print(f"Exception: {e}")
         raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
-
